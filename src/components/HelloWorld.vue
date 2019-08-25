0c6068ef3af54ecc487b8ff7f6165410b9b53a95
wenhao.guo <wenhao.guo@definesys.com>
2019-08-25 15:34:37 +0800

    win

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,32 +2,49 @@
   <div class="hello">
     <a-back-top />
     <!-- ============head ==================== -->
-    <div class="head">
+    <div id="header" class="head">
       <div class="head-content">
         <div class="head-content-head flex-row">
           <img src="/static/imgs/title.png" />
           <div class="flex-row hori-center" style="margin-left:auto">
-            <div class="nav-button" >
+            <div class="nav-button" @click="goTop()">
               <h1>首页</h1>
             </div>
-            <div class="nav-button" @click="goto(700)">
+            <div class="nav-button" @click="goAbout('header')">
               <h1>关于</h1>
             </div>
             <div class="nav-button">
               <h1>联系我们</h1>
             </div>
           </div>
-          <div class="hori-center" style="margin-left:2rem;">
-            <a-button type="primary" size="large" >登录 / 注册</a-button>
+          <div class="hori-center" style="margin-left:3rem;">
+            <a-button type="primary" size="large" @click="visible = 'true'">登录 / 注册</a-button>
+            <a-modal
+           
+              title="Title"
+              :visible="visible"
+              @ok="handleOk"
+              :confirmLoading="confirmLoading"
+              @cancel="handleCancel"
+            >
+              <div  class="modal">
+                
+              </div>
+            </a-modal>
           </div>
         </div>
-        <div class="head-content-middle">
-          我愿
-          <br/>我的脚步留痕天下
-        </div>
       </div>
-      <div style=" position: relative;">
-        <a-carousel autoplay effect="fade" dots="false">
+      <div>
+        <div class="head-content-middle flex-col">
+          <div class="text-head">Trip</div>
+          <div class="line"></div>
+          <div class="text-foot">
+            We've been traveling,
+            waiting for someone to be the partner of our journey, to accompany us through a memory that no one else can replace
+          </div>
+          <br />
+        </div>
+        <a-carousel autoplay effect="fade" :dots="false">
           <div v-for="(item,index) in imgs" :key="index">
             <img :src="item.src" width="100%" height="800px" />
           </div>
@@ -35,23 +52,58 @@
       </div>
     </div>
     <!-- ============body ==================== -->
-    <div id="body" class=" body" >
-      <div class="head body-one flex-row" style="height:600px">
-        <div>
-
-        </div>
-        <div class="body-bg">
-
+    <div id="body" class="body">
+      <div class="body-one flex-row">
+        <div class="img-bg" style="width:40%"></div>
+        <div class="body-bg flex-col" style="width:60%">
+          <div class="in-block">
+            <h3>About Me</h3>
+            <div class="line"></div>
+            <h1>仰望星空 脚踏实地</h1>
+            <div class="text-middle">
+              Dreaming is the easy part. Acting on the dream is harder.
+              Recognize that a dream is a journey. On the simplest level, it takes commitment, time, desire, and courage.
+              But rarely is something great easily realized.
+              Dreaming is recognizing and embracing the potential for greatness and seeking it in all areas of your life.
+              Believe in your dreams and your ability to accomplish them. Keep your dreams in front of you.
+              I'm here to challange you to reach for your dream.
+              Do not be afraid to dream. Never forget, if you can dream it, you can do it!
+            </div>
+          </div>
         </div>
       </div>
-      <div class="head body-two">
-
+      <div class="head flex-row">
+        <div class="body-bg2 flex-col" style="width:40%">
+          <div class="in-block">
+            <h3>STEPS</h3>
+            <div class="line"></div>
+            <h1>仰望星空 脚踏实地</h1>
+            <div class="text-middle">
+              Dreaming is the easy part. Acting on the dream is harder.
+              Recognize that a dream is a journey. On the simplest level, it takes commitment, time, desire, and courage.
+              But rarely is something great easily realized.
+              Dreaming is recognizing and embracing the potential for greatness and seeking it in all areas of your life.
+              Believe in your dreams and your ability to accomplish them. Keep your dreams in front of you.
+              I'm here to challange you to reach for your dream.
+              Do not be afraid to dream. Never forget, if you can dream it, you can do it!
+            </div>
+          </div>
+        </div>
+        <div class="img-bg2" style="width:60%"></div>
       </div>
     </div>
 
     <!-- =================foot================= -->
-    <div id="foot" class="head foot">
-
+    <div id="foot" class="foot flex-row">
+      <div class="icons-list flex-row">
+        <a-icon type="weibo-square" class="foot-icon" />
+        <a-icon type="github" class="foot-icon" />
+        <a-icon type="twitter" class="foot-icon" />
+        <a-icon type="facebook" class="foot-icon" />
+      </div>
+      <div style="margin-left:auto">
+        <p>Copyright © 2019.guowenhao. All rights reserved</p>
+      </div>
     </div>
   </div>
 </template>
@@ -69,31 +121,52 @@ export default {
         { src: "/static/imgs/banner3.jpg" },
         { src: "/static/imgs/banner4.jpg" }
       ],
+      visible :false,
+      confirmLoading :false,
       step: {
         //此数据是控制动画快慢的
         type: Number,
         default: 50
-      }
+      },
+      
     };
   },
   mounted() {
     // this.$refs.head.style.height = this.winHeight + "px";
     this.$refs.head.style.height = document.body.scrollWidth;
-    //this.changeBg();
   },
   created() {},
   methods: {
-    changeBg() {
-      // this.$refs.head.style.backgroundImage = "url('../assets/imgs/title.png')";
-      setInterval(() => {
-        this.is_bg = !this.is_bg;
-      }, 4000);
+    handleOk(e) {
+      this.ModalText = 'The modal will be closed after two seconds';
+      this.confirmLoading = true;
+      setTimeout(() => {
+        this.visible = false;
+        this.confirmLoading = false;
+      }, 2000);
     },
-
-    goto(target) {
+    handleCancel(e) {
+      console.log('Clicked cancel button');
+      this.visible = false
+    },
+    goTop() {
       let distance =
         document.documentElement.scrollTop || document.body.scrollTop; //获得当前高度
-      let step = target / 50; //每步的距离   
+
+      let step = distance / 50; //每步的距离
+      (function jump() {
+        if (distance > 0) {
+          distance -= step;
+          window.scrollTo(0, distance);
+          setTimeout(jump, 16);
+        }
+      })();
+    },
+    goAbout(target) {
+      let distance =
+        document.documentElement.scrollTop || document.body.scrollTop; //获得当前高度
+      target = document.getElementById(target).scrollHeight;
+      let step = target / 50; //每步的距离
       (function jump() {
         if (distance <= target) {
           distance += step;
@@ -108,16 +181,87 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.modal {
+  background: #00000021
+}
+.foot-icon {
+  font-size: 1.4rem;
+  margin-left: 0.7rem;
+  cursor: pointer;
+}
+.foot {
+  height: 4rem;
+  background-color: black;
+  color: #ffffff;
+  padding: 1rem 10rem;
+  justify-items: center;
+}
+.text-middle {
+  line-height: 2rem;
+  text-align: right;
+  /* padding-top: 5rem; */
+  padding: 10% 0 20% 25%;
+  margin: 0 auto;
+  font-size: 1rem;
+  font-weight: 400;
+  width: 100%;
+  color: #ffffff;
+}
+.in-block {
+  text-align: left;
+  line-height: 1.5rem;
+}
+
+h3 {
+  font-size: 3rem;
+  font-weight: 600;
+  color: #ffffff;
+}
+.body-one {
+  height: 600px;
+}
+.img-bg {
+  background-image: url("/static/imgs/banner2.jpg");
+  background-size: cover;
+}
+.img-bg2 {
+  background-image: url("/static/imgs/banner4.jpg");
+  background-size: cover;
+}
+.line {
+  border: 4px solid #ffffff;
+}
+
 .head-content-middle {
-  font-size: 4rem;
+  margin: 20% auto auto 20%;
+  line-height: 1.5rem;
+  z-index: 2;
+  position: absolute;
+}
+.text-head {
+  font-size: 7rem;
   font-weight: 500;
   color: rgb(255, 255, 255);
 }
+.text-foot {
+  width: 50%;
+  margin: 0 auto;
+  margin-top: 3rem;
+  font-size: 1rem;
+  font-weight: 400;
+  color: rgb(255, 255, 255);
+}
 .body-bg {
-  background-color: aquamarine;
+  background-color: rgb(46, 196, 182);
+  padding: 3rem 8rem 2rem 1rem;
+}
+.body-bg2 {
+  background-color: rgb(46, 196, 182);
+  padding: 9rem 8rem 2rem 1rem;
 }
 .hello {
   position: relative;
+  /* background-color: black; */
 }
 .ant-carousel >>> .slick-slide {
   text-align: center;
@@ -152,12 +296,16 @@ h1 {
   margin: 0;
 }
 .head-content {
-  z-index: 3;
+  z-index: 2;
   position: fixed;
-
   padding: 1rem;
   height: 100%;
   width: 100%;
+}
+.head-content-head {
+}
+.head-content-content {
+  position: absolute;
 }
 .flex-col {
   display: flex;
