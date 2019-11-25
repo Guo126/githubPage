6fb8b095ef2796b49163ad62f3c93b42253cfd37
Mac <mac@MacdeMacBook-Air.local>
2019-11-25 23:48:17 +0800

    mac

--- src/components/items/MyZoom.vue
+++ src/components/items/MyZoom.vue
@@ -1,18 +1,36 @@
 <template>
-    <section>
-        <div class="header">
-            
-        </div>
-    </section>
+  <section>
+    <div v-for="(item,index) in story" :key="index">
+      <div class="header">
+        <h1>{{item.name}}</h1>
+      </div>
+      <div style="color:white;text-align:left;width:60%;font-size:20px">
+        <p v-for="(per,pindex) in item.content" :key="pindex">
+            &nbsp;&nbsp;{{per}}
+            </p>
+      </div>
+    </div>
+  </section>
 </template>
 <script>
+import { story } from "./story.js";
 export default {
-    data(){
-        return{
-            menu:["Home","Movie","Music","Read","About"]
+  data() {
+    return {
+      story: story,
+      menu: [
+        {
+          lookupCode: "wuxia",
+          meaning: "武侠"
+        },
+        {
+          lookupCode: "love",
+          meaning: "爱情"
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
 
